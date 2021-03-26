8efd2cfd2b869bbe7c2c8549c8f9f7982d3a673c
group3BSE1 <dead@localhost.localdomain>
2021-03-26 15:23:40 +0300

    Assignment 2

---
+++ src/chapter2/exercise3.py
@@ -0,0 +1,4 @@
+hours = input(' Enter Hours:')
+rate = input('Enter Rate:')
+gross_pay = int(hours)*float(rate)
+print(gross_pay)
---
+++ src/chapter2/exercise5.py
@@ -0,0 +1,5 @@
+#c is for celcius
+#f farenheight
+c=input(" Enter the temperature:")
+f=((int(c)*1.8)+32)
+print(f)
