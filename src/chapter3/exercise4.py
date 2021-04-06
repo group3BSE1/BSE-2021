999049a520f1c09ada433335c11495cf4ed2a713
group3BSE1 <group3BSE1@gmail.com>
2021-04-06 12:39:31 +0300

    exercise4

--- src/chapter3/exercise4.py
+++ src/chapter3/exercise4.py
@@ -4,7 +4,7 @@ try:
     age = int(age)
     if age >= 18:
         print("You can vote")
-    elif age >= 0:
+    elif age > 0:
         print(" Too young to vote")
     else:
         print('You are a time traveller')
