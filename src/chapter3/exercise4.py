3e27604c7209deaac1d0513b575090d7c0f27c1a
group3BSE1 <group3BSE1@gmail.com>
2021-04-06 12:40:23 +0300

    exercise4

---
+++ src/chapter3/exercise4.py
@@ -0,0 +1,14 @@
+# getting age from user
+age = input('Enter age:')
+try:
+    age = int(age)
+    if age >= 18:
+        print("You can vote")
+    elif age >= 0:
+        print(" Too young to vote")
+    else:
+        print('You are a time traveller')
+except:
+    age = str(age)
+    print('Please enter only integers')
+    exit()
