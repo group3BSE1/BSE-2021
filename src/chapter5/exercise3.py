437478eb2d434351a8db24ce6880a493ba38f707
group3BSE1 <group3BSE1@gmail.com>
2021-04-22 11:00:05 +0300

    exercise3-step one

---
+++ src/chapter5/exercise3.py
@@ -0,0 +1,25 @@
+# 22nd April 2021
+#GROUP 3
+# DAN KABAGAMBE
+# TUMUSIIME FRANCIS
+# NAKUYA SHAKIRAH HADIJJAH
+nickel = 25
+dime = 25
+quarter = 25
+one = 0
+five = 0
+print(nickel, "nickel")
+print(dime, "dime")
+print(quarter, "quarter")
+print(one, "one")
+print(five, "five")
+# Reapeatedly asking user to enter price
+while True:
+    price = input("Enter the purchase price (xx.xx) or `q' to quit:")
+    #do something here
+    if price == 'q':
+        quit()
+    else:
+        continue
+
+
