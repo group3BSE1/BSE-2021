a6ba3bfbaa76348a1d9a99e99f3e7d2529a398fb
group3BSE1 <group3BSE1@gmail.com>
2021-04-22 11:30:24 +0300

    getting selection from user and printing amount due

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -32,6 +32,76 @@ while True:
             print(' "o" - deposit a one dollar bill')
             print(' "f" - deposit a five dollar bill ')
             print(' "c" - cancel the purchase ')
+            dollars = amount // 100
+            cent_s = amount % 100
+            print('Amount due:',dollars,' dollars and ', cent_s,' cents')
+            payment = 0
+            # Getting the selection from user
+            while True:
+                deposit = input('Indicate your selection:')
+                if deposit == 'n':
+                    while True:
+                        payment = payment + 5
+                        amount_due = amount - payment
+                        break
+                    if payment < amount:
+                        print(amount_due//100,'dollars and' ,amount_due % 100, 'cents')
+                        continue
+                    else:
+                        print('Take change')
+                        break
+                if deposit == 'd':
+                    while True:
+                        payment = payment + 10
+                        amount_due = amount - payment
+                        break
+                    if payment < amount:
+                        print(amount_due//100,'dollars and' ,amount_due % 100, 'cents')
+                        continue
+                    else:
+                        break
+                if deposit == 'q':
+                    while True:
+                        payment = payment + 25
+                        amount_due = amount - payment
+                        break
+                    if payment < amount:
+                        print(amount_due//100,'dollars and' ,amount_due % 100, 'cents')
+                        continue
+                    else:
+                        print('Take the change')
+                if deposit == 'o':
+                    while True:
+                        payment = payment + 100
+                        amount_due = amount - payment
+                        break
+                    if payment < amount:
+                        print(amount_due//100,'dollars and' ,amount_due % 100, 'cents')
+                        continue
+                    else:
+                        print('Take change')
+                        break
+                if deposit == 'f':
+                    while True:
+                        payment = payment + 500
+                        amount_due = amount - payment
+                        break
+                    if payment < amount:
+                        print(amount_due//100,'dollars and' ,amount_due % 100, 'cents')
+                        continue
+                    else:
+                        print('Take change')
+                        break
+                if deposit == 'c':
+                    if payment > 0:
+                        print('Please take the change below')
+                        break
+                    else:
+                        break
+                else:
+                    print('Invalid')
+                    continue
+
         else:
             print('Illegal selection: Must be a non negative multiple of 5 cents')
         continue
