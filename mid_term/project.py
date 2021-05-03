dd367c63672edffe0e970b1b4de8a6e3d207eda0
group3BSE1 <group3BSE1@gmail.com>
2021-05-03 23:21:40 +0300

    prompting for meter readings

--- mid_term/project.py
+++ mid_term/project.py
@@ -2,7 +2,13 @@
 while True:
     code = input('Enter customer code:')
     if code == "r" or code == "c" or code == "i":
+# bgm_m is the beginning meter reading
+# end_m is the ending meter reading
+        bgn_m = input('Enter beginning meter reading:')
+        end_m = input('Enter ending meter reading:')
         print("Customer code: ", code)
+        print('Beginning meter reading: ', bgn_m)
+        print('Ending meter reading: ', end_m)
         continue
     # quiting the program if customer code is invalid
     else:
