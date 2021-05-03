6b4f1a3800d136bf4b4f81ba97aeadc43f8c4318
group3BSE1 <group3BSE1@gmail.com>
2021-05-03 23:11:51 +0300

    calcilating bill

--- mid_term/project.py
+++ mid_term/project.py
@@ -1,22 +1,44 @@
 # repeatedly getting customer code
 while True:
     code = input('Enter customer code:')
-    if code == "r" or code == "c" or code == "i":
+    if code == 'r' or code == 'c' or code == 'i':
         # bgn_m is the beginning meter reading
         # end_m is the ending meter reading
         bgn_m = input('Enter beginning meter reading:')
         end_m = input('Enter ending meter reading:')
         # when end_m < bgn_m, the meter reading exceeded 9 digits
         # end_m2 is what we shall use when calculating
+
         if end_m < bgn_m:
             end_m2 = 1000000000 + int(end_m)
         else:
             end_m2 = end_m
         gallons = (int(end_m2) - int(bgn_m))/10
+        # calculating bill for code 'r'
+        if code == 'r':
+            bill = format((5+(0.0005 * gallons)), ".2f")
+        # calculating bill for code 'c'
+        elif code == 'c':
+            if gallons <= 4000000:
+                bill = format(1000, ".2f")
+            else:
+                additional_g = gallons - 4000000
+                bill = format(((0.00025 * additional_g) + 1000.00), ".2f")
+        # calculating bill for code 'i'
+        else:
+            if gallons <= 4000000:
+                bill = format(1000, ".2f")
+            elif gallons <= 10000000:
+                bill = format(2000, ".2f")
+            else:
+                exceeded_g = gallons - 10000000
+                bill = format(((0.00025 * exceeded_g) + 2000), ".2f")
+
         print("Customer code: ", code)
         print('Beginning meter reading: ', bgn_m)
         print('Ending meter reading: ', int(end_m))
         print('Gallons of water used: ', gallons)
+        print('Amount billed: '+'$'+str(bill))
         continue
     # quiting the program if customer code is invalid
     else:
