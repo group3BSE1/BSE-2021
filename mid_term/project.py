34a5c87b3ce1e62b0ddde0b8fcdcaeee88610164
group3BSE1 <group3BSE1@gmail.com>
2021-05-05 11:41:24 +0300

    trapping invalid user-supplied meter readings

--- mid_term/project.py
+++ mid_term/project.py
@@ -8,12 +8,22 @@ while True:
         end_m = input('Enter ending meter reading:')
         # when end_m < bgn_m, the meter reading exceeded 9 digits
         # end_m2 is what we shall use when calculating
-
-        if end_m < bgn_m:
-            end_m2 = 1000000000 + int(end_m)
-        else:
-            end_m2 = end_m
-        gallons = (int(end_m2) - int(bgn_m))/10
+        # trapping invalid supplied user meter readings
+        try:
+            bgn_m = int(bgn_m)
+            end_m = int(end_m)
+            if bgn_m >= 0 and end_m >= 0 and len(str(bgn_m)) <= 9 and len(str(end_m)) <= 9:
+                if end_m < bgn_m:
+                    end_m2 = 1000000000 + int(end_m)
+                else:
+                    end_m2 = end_m
+            else:
+                print('Invalid meter reading')
+                continue
+        except:
+            print('Invalid meter reading')
+            continue
+        gallons = (int(end_m2) - int(bgn_m)) / 10
         # calculating bill for code 'r'
         if code == 'r':
             bill = format((5+(0.0005 * gallons)), ".2f")
