5a06b9519ee23f88ba787e410f1924e064df8091
group3BSE1 <group3BSE1@gmail.com>
2021-05-16 13:30:52 +0300

    exercise1

--- src/chapter6/exercise1.py
+++ src/chapter6/exercise1.py
@@ -1,6 +1,6 @@
 # a while loop to print da word banana starting from the last letter
 fruit = 'banana'
-index = 5
+index = (len(fruit)-1)
 while index < len(fruit):
     if index >= 0:
         letter = fruit[index]
