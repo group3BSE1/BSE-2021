f31db3fc2b90330af6d4787fe9aae56be50c0dbe
group3BSE1 <group3BSE1@gmail.com>
2021-05-22 02:30:23 +0300

    project2_b.py

--- src/chapter8/project2_b.py
+++ src/chapter8/project2_b.py
@@ -15,6 +15,7 @@ def open_file():
 
 def process_file(file_object):
     # getting user inputs
+    global min_country, max_country
     year = input('Enter the year: ')
     while True:
         income_level = input('Enter income level:')
@@ -32,16 +33,50 @@ def process_file(file_object):
     ilevel = leves[income_level - 1]
     count = 0
     percentages = []
+    list_criteria = []
     for line in file_object:
         # line[88:93] is the slice having the year and line[51:57] is one having the income level
         if line[88:93].startswith(year) and ilevel in line[51:57]:
             count = count + 1
             # percent_no is a variable to hold the percentage value
             percent_no = line[59:61]
+            list_criteria.append(line)
             # putting the percentages in a list
             percentages.append(int(percent_no))
+            max_country = str(max(percentages))
+            min_country = str(min(percentages))
+    list_max_countries_nw = []
+    list_min_countries_nw = []
+    for line in list_criteria:
+        if max_country in line:
+            list_max_countries_nw.append(line[:45])
+    for line in list_criteria:
+        if min_country in line:
+            list_min_countries_nw.append(line[:45])
 
+    # a function to get the most frequent country with max or min percentage
+    def most_frequent_country(list_country):
+        counter = 0
+        country_name = list_country[0]
+        for i in list_country:
+            current_frequency = list_country.count(i)
+            if current_frequency > counter:
+                counter = current_frequency
+                country_name = i
+        return country_name
+    country_most = most_frequent_country(list_max_countries_nw)
+    countryless = most_frequent_country(list_min_countries_nw)
+
+    
+    # Displaying a report to the user
+    print('Count of records: ', count)
+    print('Average percentage of children vaccinated: ', format((sum(percentages)/count), ".1f"))
+    print('Maximum percrntage: ', max(percentages), country_most)
+    print('Minimum percentage: ', min(percentages), countryless)
 
 
 def main():
     process_file(open_file())
+
+
+main()
