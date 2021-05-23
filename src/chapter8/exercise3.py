4118bd3f1de07e0beae7f6a7e24ab6293dd3b78c
group3BSE1 <group3BSE1@gmail.com>
2021-05-23 22:21:37 +0300

    exercise3

--- src/chapter8/exercise3.py
+++ src/chapter8/exercise3.py
@@ -3,6 +3,5 @@ count = 0
 for line in fhand:
     words = line.split()
     # print 'Debug', words
-    if len(words) == 0:continue
-    if words[0] != 'From' and len(words) < 3: continue
+    if len(words) == 0 or words[0] != 'From' or len(words) < 3: continue
     print(words[2])  # this line can cause the program to fail if not guarded against
