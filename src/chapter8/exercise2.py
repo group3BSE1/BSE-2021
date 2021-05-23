a68876109116f73f1fa6864ffc7e8045f5084ae2
group3BSE1 <group3BSE1@gmail.com>
2021-05-23 22:21:15 +0300

    exercise2

--- src/chapter8/exercise2.py
+++ src/chapter8/exercise2.py
@@ -3,10 +3,7 @@ count = 0
 for line in fhand:
     words = line.split()
     # print 'Debug', words
-    if len(words) == 0:continue
-    if words[0] != 'From': continue
-    if len(words) >=3:
-        print(words[2])  # this line can cause the program to fail if not guarded against
-    else:
-        print('Error')
-        quit()
+    if len(words) == 0: continue
+    # we guard the word[2] by ensuring the length of word is greater or equal to 3
+    if words[0] != 'From' or len(words) < 3: continue
+    print(words[2])   # this line can cause the program to fail if not guarded against
