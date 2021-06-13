c4706c08d5c422c5ce3d17c2ce831dfe582339bc
group3BSE1 <group3BSE1@gmail.com>
2021-06-14 01:59:25 +0300

    exercise1

--- src/chapter9/exercise1.py
+++ src/chapter9/exercise1.py
@@ -1,8 +1,8 @@
 fhand = open('words.txt')
 d = dict()
-for words in fhand:
-    line = words.split()
-    for word in line:
+for line in fhand:
+    words = line.split()
+    for word in words:
         d[word] = d.get(word, 0) + 1
 
 if 'programs' in d:
